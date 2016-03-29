4d02938559f47747fc0885bd43863b47f1943cff
Pawneet Singh <pawneet.dev@gmail.com>
2016-03-29 16:45:26 +0530

    Index action added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 GmailAlerts::Application.routes.draw do
+
   root to: 'sessions#new'
   resources :sessions, only: :index
   get "/auth/:provider/callback" => 'sessions#create'
+
+end
+
+
+Rails.application.routes.draw do
+  match "/sessions/index" => "sessions#index", :via => [:get, :post]
 end
